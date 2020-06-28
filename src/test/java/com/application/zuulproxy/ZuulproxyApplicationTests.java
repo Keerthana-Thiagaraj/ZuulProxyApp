ee20d89dec59a572aae18200fd01f5614e048f30
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-06-28 16:31:12 +0530

    Zuul proxy enabled for communication b/w services

---
+++ src/test/java/com/application/zuulproxy/ZuulproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.zuulproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
